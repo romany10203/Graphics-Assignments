dfc13e30b36524327a8b92b3b7145217001f7666
Romany Moner <romanym2020@gmail.com>
2022-04-16 22:48:36 +0200

    Graphics-Assignments
    Graphics-Assignments

---
+++ 3D drawing/Graphics/GraphicsForm.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Forms;
+using System.Threading;
+namespace Graphics
+{
+    public partial class GraphicsForm : Form
+    {
+        Renderer renderer = new Renderer();
+        Thread MainLoopThread;
+        public GraphicsForm()
+        {
+            InitializeComponent();
+            simpleOpenGlControl1.InitializeContexts();
+            initialize();
+            MainLoopThread = new Thread(MainLoop);
+            MainLoopThread.Start();
+
+        }
+        void initialize()
+        {
+            renderer.Initialize();   
+        }
+        
+        void MainLoop()
+        {
+            while (true)
+            {
+                renderer.Update();
+                renderer.Draw();
+                simpleOpenGlControl1.Refresh();
+            }
+        }
+        private void GraphicsForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            renderer.CleanUp();
+            MainLoopThread.Abort();
+        }
+
+        private void simpleOpenGlControl1_Paint(object sender, PaintEventArgs e)
+        {
+            renderer.Draw();
+        }
+
+    }
+}
